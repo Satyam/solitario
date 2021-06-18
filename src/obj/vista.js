f6bd31e512f4c6426d77febae8ed8d2185e062db
Satyam <satyam@satyam.com.ar>
2021-06-18 19:11:51 +0200

    Added code in `vista` to drag away a card via `juego`

--- src/obj/vista.js
+++ src/obj/vista.js
@@ -1,23 +1,43 @@
-import { MAZO, VISTA, HUECO } from '../datos.js';
 import k from '../k.js';
 import grilla from '../comp/grilla.js';
+import { MAZO, VISTA, HUECO, JUEGO } from '../datos.js';
 
 export function vistaComp() {
   const cartas = [];
+  let dragSensed = false;
 
   return {
     add() {
       k.on('popped', MAZO, (_, id) => {
-        console.log('en vista', id);
-        cartas.push(id);
-        this.changeSprite(id);
+        this.push(id);
       });
+      k.mouseDown(() => {
+        if (dragSensed || cartas.length === 0 || !this.hasPt(k.mousePos()))
+          return;
+        dragSensed = true;
+        const j = k.get(JUEGO)[0];
+        if (j.grab(this.getTop())) {
+          cartas.pop();
+          this.changeSprite(this.getTop() || HUECO);
+        }
+      });
+      k.mouseRelease(() => {
+        dragSensed = false;
+      });
+    },
+    push(id) {
+      cartas.push(id);
+      this.changeSprite(id);
     },
     vuelta() {
       const ret = Array.from(cartas).reverse();
       cartas.length = 0;
       this.changeSprite(HUECO);
       return ret;
+    },
+    getTop() {
+      const l = cartas.length;
+      return l ? cartas[l - 1] : null;
     },
   };
 }
