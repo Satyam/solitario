51363c9e025fc100e9aded94a38b3978c6f2dcd8
Satyam <satyam@satyam.com.ar>
2021-07-06 17:38:41 +0200

    Initial install of Phaser3

--- src/loadSprites.js
+++
@@ -1,9 +0,0 @@
-import k from './k.js';
-import { baraja, HUECO, REVERSO } from './datos.js';
-
-Object.keys(baraja).forEach((cardId) =>
-  k.loadSprite(cardId, `cards/${cardId}.svg`)
-);
-
-k.loadSprite(REVERSO, 'cards/2B.svg');
-k.loadSprite(HUECO, 'src/hueco.svg');
