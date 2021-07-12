002fcf2fb5d0953f35fa42b9c16c7317620909e3
Satyam <satyam@satyam.com.ar>
2021-07-12 17:21:21 +0200

    Dropped Bulma CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import './App.css';
-import 'bulma/css/bulma.min.css';
-import Navbar from './Navbar';
 import Mazo from 'Components/Mazo';
 import Vista from 'Components/Vista';
 import Hueco from 'Components/Hueco';
@@ -16,35 +14,29 @@ function App() {
   return (
     <div className="App">
       <header>
-        <Navbar />
+        <button>Nuevo Juego</button>
       </header>
       <main>
-        <div className="columns">
-          <div className="column">
-            <div className="celda">
-              <Mazo />
-            </div>
+        <div className="row">
+          <div className="celda">
+            <Mazo />
           </div>
-          <div className="column">
-            <div className="celda">
-              <Vista />
-            </div>
+
+          <div className="celda">
+            <Vista />
           </div>
-          <div className="column"></div>
+
+          <div className="celda">&nbsp;</div>
           {[0, 1, 2, 3].map((slot) => (
-            <div key={slot} className="column">
-              <div className="celda">
-                <Pila slot={slot} />
-              </div>
+            <div key={slot} className="celda">
+              <Pila slot={slot} />
             </div>
           ))}
         </div>
-        <div className="columns">
+        <div className="row">
           {[0, 1, 2, 3, 4, 5, 6].map((slot) => (
-            <div key={slot} className="column">
-              <div className="celda">
-                <Hueco slot={slot} />
-              </div>
+            <div key={slot} className="celda">
+              <Hueco slot={slot} />
             </div>
           ))}
         </div>
