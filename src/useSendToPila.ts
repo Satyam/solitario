c02a70740cae4306071ad34edd7126dab75ed763
Satyam <satyam@satyam.com.ar>
2021-07-15 13:54:48 +0200

    Created global set selectors to do operations involving several slots

--- src/useSendToPila.ts
+++ src/useSendToPila.ts
@@ -1,43 +1,21 @@
-import { CardId, baraja } from 'datos';
+import { CardId } from 'datos';
 import { PointerEventHandler } from 'react';
-import { pilaState } from 'store/pilas';
-import { useRecoilState } from 'recoil';
+import { sendToPila } from 'store/globals';
+import { useSetRecoilState } from 'recoil';
 
 export function useSendToPila(
   cardId: CardId,
   dropper: () => void
 ): PointerEventHandler {
-  const p = [
-    useRecoilState(pilaState(0)),
-    useRecoilState(pilaState(1)),
-    useRecoilState(pilaState(2)),
-    useRecoilState(pilaState(3)),
-  ];
+  const send = useSetRecoilState(sendToPila);
   return (ev) => {
     if (ev.buttons === 4) {
       ev.preventDefault();
       console.log('vista to pila', cardId);
-      const droppedCarta = baraja[cardId];
-      for (let slot = 0; slot < 4; slot++) {
-        const [cardIds, setCardIds] = p[slot];
-        if (cardIds.length) {
-          const topCarta = baraja[cardIds[0]];
-          if (
-            droppedCarta.index === topCarta.index + 1 &&
-            droppedCarta.palo === topCarta.palo
-          ) {
-            setCardIds([cardId, ...cardIds]);
-            dropper();
-            break;
-          }
-        } else {
-          if (droppedCarta.index === 0) {
-            setCardIds([cardId, ...cardIds]);
-            dropper();
-            break;
-          }
-        }
-      }
+      try {
+        send(cardId);
+        dropper();
+      } catch (err) {}
     }
   };
 }
