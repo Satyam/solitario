ba89bbc83928a562a81dc971be35ef22470d4ec2
Satyam <satyam@satyam.com.ar>
2021-07-15 13:54:48 +0200

    Created global set selectors to do operations involving several slots

---
+++ src/store/globals.ts
@@ -0,0 +1,77 @@
+import { mazoState } from 'store/mazo';
+import { huecoState, firstShownState } from 'store/huecos';
+import { vistaState } from 'store/vista';
+import { pilaState } from 'store/pilas';
+import { DefaultValue, selector } from 'recoil';
+import { getRandomInt } from 'utils';
+import {
+  CardId,
+  numCartas,
+  numPalos,
+  numValores,
+  valores,
+  palos,
+  baraja,
+  REVERSO,
+} from 'datos';
+
+export const init = selector({
+  key: 'initSelector',
+  get: () => undefined,
+  set: ({ set, reset }) => {
+    reset(vistaState);
+    for (let slot = 0; slot < 4; slot++) {
+      reset(pilaState(slot));
+    }
+
+    const cardIds: CardId[] = [];
+    while (cardIds.length < numCartas) {
+      let p = getRandomInt(numPalos - 1);
+      let v = getRandomInt(numValores - 1);
+      const cardId = `${valores[v]}${palos[p]}` as CardId;
+      if (!cardIds.includes(cardId)) {
+        cardIds.push(cardId);
+      }
+    }
+    debugger;
+    for (let slot = 0; slot < 7; slot++) {
+      set(firstShownState(slot), slot);
+      set(huecoState(slot), cardIds.splice(0, slot + 1));
+    }
+    set(mazoState, cardIds);
+  },
+});
+
+export const sendToPila = selector<CardId>({
+  key: 'sendToPilaSelector',
+  get: () => REVERSO,
+  set: ({ get, set }, cardId) => {
+    if (cardId instanceof DefaultValue) return;
+    const droppedCarta = baraja[cardId];
+    let done = false;
+    for (let slot = 0; slot < 4; slot++) {
+      const cardIds = get(pilaState(slot));
+      if (cardIds.length) {
+        const topCarta = baraja[cardIds[0]];
+        if (
+          droppedCarta.index === topCarta.index + 1 &&
+          droppedCarta.palo === topCarta.palo
+        ) {
+          set(pilaState(slot), [cardId, ...cardIds]);
+          done = true;
+          break;
+        }
+      } else {
+        if (droppedCarta.index === 0) {
+          set(pilaState(slot), [cardId, ...cardIds]);
+          done = true;
+          break;
+        }
+      }
+    }
+    if (!done) {
+      console.error('cannot set');
+      throw new Error('cannot set');
+    }
+  },
+});
--- src/useInit.ts
+++ src/useInit.ts
@@ -1,53 +1,14 @@
-import { mazoState } from 'store/mazo';
-import { huecoState, firstShownState } from 'store/huecos';
-import { vistaState } from 'store/vista';
-import { pilaState } from 'store/pilas';
-import { useResetRecoilState, useSetRecoilState } from 'recoil';
-import { getRandomInt } from 'utils';
-import {
-  CardId,
-  numCartas,
-  numPalos,
-  numValores,
-  valores,
-  palos,
-  baraja,
-} from 'datos';
+import { init } from 'store/globals';
+import { useSetRecoilState } from 'recoil';
+import { baraja } from 'datos';
 import { useState } from 'react';
 
 const useInit = () => {
   const [firstTime, setFirstTime] = useState(true);
-  const setMazo = useSetRecoilState(mazoState);
-  const setHueco = [
-    useSetRecoilState(huecoState(0)),
-    useSetRecoilState(huecoState(1)),
-    useSetRecoilState(huecoState(2)),
-    useSetRecoilState(huecoState(3)),
-    useSetRecoilState(huecoState(4)),
-    useSetRecoilState(huecoState(5)),
-    useSetRecoilState(huecoState(6)),
-  ];
-
-  const setFirstShown = [
-    useSetRecoilState(firstShownState(0)),
-    useSetRecoilState(firstShownState(1)),
-    useSetRecoilState(firstShownState(2)),
-    useSetRecoilState(firstShownState(3)),
-    useSetRecoilState(firstShownState(4)),
-    useSetRecoilState(firstShownState(5)),
-    useSetRecoilState(firstShownState(6)),
-  ];
-
-  const resetVista = useResetRecoilState(vistaState);
-
-  const resetPila = [
-    useResetRecoilState(pilaState(0)),
-    useResetRecoilState(pilaState(1)),
-    useResetRecoilState(pilaState(2)),
-    useResetRecoilState(pilaState(3)),
-  ];
-
+  const resetState = useSetRecoilState(init);
   return () => {
+    resetState(undefined);
+
     if (firstTime) {
       setFirstTime(false);
       // Image preload
@@ -55,26 +16,6 @@ const useInit = () => {
         new Image().src = `assets/cards/${cardId}.svg`;
       });
     }
-
-    resetVista();
-    resetPila.forEach((r) => {
-      r();
-    });
-    const cardIds: CardId[] = [];
-    while (cardIds.length < numCartas) {
-      let p = getRandomInt(numPalos - 1);
-      let v = getRandomInt(numValores - 1);
-      const cardId = `${valores[v]}${palos[p]}` as CardId;
-      if (!cardIds.includes(cardId)) {
-        cardIds.push(cardId);
-      }
-    }
-
-    for (let slot = 0; slot < 7; slot++) {
-      setFirstShown[slot](slot);
-      setHueco[slot](cardIds.splice(0, slot + 1));
-    }
-    setMazo(cardIds);
   };
 };
 
