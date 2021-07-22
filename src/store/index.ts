4b81070ff820d84fe078a535ca0323942b26e07c
Satyam <satyam@satyam.com.ar>
2021-07-22 14:21:14 +0200

    More switching to re-exporting `*`

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,15 +2,7 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { tAppDispatch, tRootState } from 'store/store';
 
 export { store } from 'store/store';
-export {
-  newGameAction,
-  jugadaAction,
-  restoreMazoAction,
-  undoAction,
-  redoAction,
-  clearUndoAction,
-} from 'store/juegoSlice';
-
+export * from 'store/juegoSlice';
 export * from 'store/selectors';
 
 export const useAppDispatch = () => useDispatch<tAppDispatch>();
