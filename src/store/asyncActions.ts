a042a642c4337a604ce9ca94c9602e8bb97d0880
Satyam <satyam@satyam.com.ar>
2021-07-30 17:56:53 +0200

    At the end of a game, it animates the cards falling down

--- src/store/asyncActions.ts
+++ src/store/asyncActions.ts
@@ -6,16 +6,20 @@ import {
   SPRITE_ID,
   ANIM_DURATION,
   tCardId,
+  numPilas,
 } from 'datos';
+
 import {
   tAppDispatch,
   tRootState,
   selPilaToSendCard,
   selCoords,
+  selHasWon,
   takeFrom,
   putInto,
 } from 'store';
 import { sleep } from 'utils';
+import { selPila } from './selectors';
 
 export const raiseAction = createAsyncThunk<
   void,
@@ -74,13 +78,14 @@ const moveCarta = async (
     console.error(`Card element with id ${SPRITE_ID} not found`);
     return Promise.reject(`Card element with id ${SPRITE_ID} not found`);
   }
+
   spriteEl.src = `assets/cards/${cardId}.svg`;
   spriteEl.style.top = `${fromTop}px`;
   spriteEl.style.left = `${fromLeft}px`;
   spriteEl.style.display = 'inline-block';
   // Mozilla docs suggests allowing a handful of ms after element is displayed
   // before starting transformation
-  await sleep(10);
+  await sleep(20);
   spriteEl.style.transform = `translate(${toLeft - fromLeft}px, ${
     toTop - fromTop
   }px)`;
@@ -94,6 +99,7 @@ const moveCarta = async (
   });
   spriteEl.style.display = 'none';
   spriteEl.style.transform = 'none';
+  spriteEl.style.transition = 'unset';
 };
 
 export const jugadaAction = createAsyncThunk<
@@ -118,4 +124,38 @@ export const jugadaAction = createAsyncThunk<
   }
 
   dispatch(putInto(jugada));
+  if (selHasWon(getState())) {
+    await sleep(300);
+    await dispatch(winAnimation());
+  }
+});
+
+export const winAnimation = createAsyncThunk<
+  void,
+  void,
+  { dispatch: tAppDispatch; state: tRootState }
+>('winAnimation', async (_, { dispatch, getState }) => {
+  const { width, bottom } = document.body.getBoundingClientRect();
+
+  let count: number;
+  do {
+    count = 0;
+    for (let slot = 0; slot < numPilas; slot++) {
+      const cardIds = selPila(getState(), slot);
+      if (cardIds.length > 0) {
+        count += 1;
+        dispatch(
+          takeFrom({
+            cardIds: [cardIds[0]],
+            toPos: POS.MAZO,
+            toSlot: 0,
+            fromPos: POS.PILA,
+            fromSlot: slot,
+          })
+        );
+        const { left, top } = selCoords(getState(), `${POS.PILA}${slot}`);
+        await moveCarta(cardIds[0], top, left, bottom, width / 2);
+      }
+    }
+  } while (count);
 });
