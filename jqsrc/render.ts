86c4da09e52852805c31cae05c7793a9aa93f695
Satyam <satyam@satyam.com.ar>
2021-08-13 09:55:03 +0200

    Prevent unnecesary re-renders when top card is the same

--- jqsrc/render.ts
+++ jqsrc/render.ts
@@ -1,22 +1,44 @@
-import { REVERSO, HUECO, SEL, datos, tCardId } from './datos.js';
+import {
+  REVERSO,
+  HUECO,
+  SEL,
+  datos,
+  tCardId,
+  numPilas,
+  numHuecos,
+} from './datos.js';
 import { imgSrc, cardImg } from './utils.js';
+
+let topMazo: tCardId;
+let topVista: tCardId;
+const topPilas: tCardId[] = Array(numPilas);
+const topHuecos: tCardId[] = Array(numHuecos);
 
 const setCardId = (el: JQuery, cardId: tCardId) =>
   el.find('img').prop('src', imgSrc(cardId));
 
-export const renderMazo = () =>
-  setCardId($(SEL.MAZO), datos.mazo.length ? REVERSO : HUECO);
+export const renderMazo = () => {
+  const cardId = datos.mazo.length ? REVERSO : HUECO;
+  if (cardId === topMazo) return;
+  topMazo = cardId;
+  setCardId($(SEL.MAZO), cardId);
+};
 
-export const renderVista = () =>
-  setCardId($(SEL.VISTA), datos.vista[0] || HUECO);
+export const renderVista = () => {
+  const cardId = datos.vista[0] || HUECO;
+  if (cardId === topVista) return;
+  topVista = cardId;
+  setCardId($(SEL.VISTA), cardId);
+};
 
-export const renderPilas = () =>
-  $(SEL.PILAS).each(function (slot) {
-    setCardId($(this), datos.pilas[slot][0] || HUECO);
-  });
+export const renderPila = (slot: number) => {
+  const cardId = datos.pilas[slot][0] || HUECO;
+  if (cardId === topPilas[slot]) return;
+  topPilas[slot] = cardId;
+  setCardId($(SEL.PILAS), cardId);
+};
 
-export const renderPila = (slot: number) =>
-  setCardId($(SEL.PILAS), datos.pilas[slot][0] || HUECO);
+export const renderPilas = () => $(SEL.PILAS).each(renderPila);
 
 const renderHuecoStack = (
   cardIds: tCardId[],
@@ -34,17 +56,22 @@ const renderHuecoStack = (
 
 const renderOneHueco = (h: JQuery, slot: number) => {
   const cardIds = datos.huecos[slot];
+  const cardId = cardIds[0];
+  if (cardId === topHuecos[slot]) return;
+  topHuecos[slot] = cardId;
   h.html(
     cardIds.length
       ? renderHuecoStack(cardIds, datos.firstShown[slot], 0)
       : cardImg(HUECO)
   );
 };
+
 export const renderHuecos = () => {
   $(SEL.HUECOS).each(function (slot) {
     renderOneHueco($(this), slot);
   });
 };
+
 export const renderHueco = (slot: number) => {
   renderOneHueco($(SEL.HUECOS).eq(slot), slot);
 };
