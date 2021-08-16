f120707f14e4d0c68c0fa4add6ee1e6552eb82a9
Satyam <satyam@satyam.com.ar>
2021-08-16 18:16:20 +0200

    Added `fixFirstShown` to utils to use when a hueco shrinks

--- jqsrc/utils.ts
+++ jqsrc/utils.ts
@@ -1,6 +1,15 @@
+import { datos } from './datos.js';
+
 // export const sleep = (ms: number) =>
 //   new Promise<void>((resolve) => setTimeout(() => resolve(), ms));
 
 export function shuffle<T extends any[]>(a: T): T {
   return a.sort(() => Math.random() - 0.5);
+}
+
+export function fixFirstShown(slot: number) {
+  const lastCardIndex = datos.huecos[slot].length - 1;
+  if (datos.firstShown[slot] > lastCardIndex) {
+    datos.firstShown[slot] = lastCardIndex;
+  }
 }
