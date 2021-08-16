153494bbd0a2ee3165b7ee741103a0f1f278140a
Satyam <satyam@satyam.com.ar>
2021-08-16 18:13:27 +0200

    Added quick wheel button raise cards

--- jqsrc/actions.ts
+++ jqsrc/actions.ts
@@ -1,7 +1,21 @@
-import { SEL, datos, tCardId, baraja, numHuecos, numPilas } from './datos.js';
-import { renderAll, renderMazo, renderVista } from './render.js';
+import {
+  SEL,
+  datos,
+  tCardId,
+  baraja,
+  numHuecos,
+  numPilas,
+  tCarta,
+} from './datos.js';
+import {
+  renderAll,
+  renderMazo,
+  renderPila,
+  renderVista,
+  renderHueco,
+} from './render.js';
 import { initUndo, pushState } from './undoStack.js';
-import { shuffle } from './utils.js';
+import { shuffle, fixFirstShown } from './utils.js';
 
 export const initActions = () => {
   // Buttons
@@ -12,6 +26,8 @@ export const initActions = () => {
 
   // cards
   $(SEL.MAZO).on('click', dealCard);
+  $(SEL.VISTA).on('mousedown', vistaToPila);
+  $(SEL.HUECOS).on('mousedown', huecoToPila);
 };
 
 export const startNewGame = (): void => {
@@ -43,3 +59,50 @@ const dealCard = (ev: JQuery.Event) => {
   renderMazo();
   renderVista();
 };
+
+function canDropInPila(fromCarta: tCarta) {
+  if (typeof fromCarta === 'undefined') return -1;
+  for (let slot = 0; slot < numPilas; slot++) {
+    const toCarta = baraja[datos.pilas[slot][0]];
+    if (toCarta) {
+      if (
+        fromCarta.palo === toCarta.palo &&
+        fromCarta.index === toCarta.index + 1
+      )
+        return slot;
+    } else {
+      if (fromCarta.valor === 'A') return slot;
+    }
+  }
+  return -1;
+}
+
+function vistaToPila(ev: JQuery.Event) {
+  if (ev.buttons === 4) {
+    const slot = canDropInPila(baraja[datos.vista[0]]);
+    if (slot >= 0) {
+      pushState();
+      datos.pilas[slot].unshift(datos.vista.shift());
+      renderVista();
+      renderPila(slot);
+    }
+    // stop propagation
+    return false;
+  }
+}
+
+function huecoToPila(ev: JQuery.Event) {
+  if (ev.buttons === 4) {
+    const fromSlot = $(this).data('slot');
+    const toSlot = canDropInPila(baraja[datos.huecos[fromSlot][0]]);
+    if (toSlot >= 0) {
+      pushState();
+      datos.pilas[toSlot].unshift(datos.huecos[fromSlot].shift());
+      fixFirstShown(fromSlot);
+      renderHueco(fromSlot);
+      renderPila(toSlot);
+    }
+    // stop propagation
+    return false;
+  }
+}
