4ab911e81305f1fca2ae2d67ffd3b6a6ad891807
Satyam <satyam@satyam.com.ar>
2021-09-13 17:08:31 +0200

    spline: made more options of movement. Simplyfied much

--- public/spline/index.js
+++ public/spline/index.js
@@ -1,18 +1,13 @@
 const NUM_STEPS = 8;
 const FULL_TURN = 2 * Math.PI;
 const STEP = FULL_TURN / NUM_STEPS;
-const MAX_RADIUS = 100;
-
-const START_POINT_RADIUS = MAX_RADIUS * 0.1;
-const START_CONTROL_POINT_RADIUS = MAX_RADIUS * 0.2;
-const END_POINT_RADIUS = MAX_RADIUS;
-const END_CONTROL_POINT_RADIUS = MAX_RADIUS * 0.9;
 
 const angles = Array(NUM_STEPS)
   .fill(0)
   .map((_, i) => i * STEP);
 
-const rexp = /M\s+-?\d+,-?\d+\s+(.+)/;
+const regex =
+  /^M\s+(-?\d+,-?\d+)\s+C\s+(-?\d+,-?\d+)\s+(-?\d+,-?\d+)\s+(-?\d+,-?\d+)$/g;
 
 window.onload = () => {
   const svgEl = document.getElementById('svg');
@@ -38,12 +33,12 @@ window.onload = () => {
 
   resizeHandler();
 
-  const shortest = Math.min(vb.height, vb.width);
+  const shortest = Math.min(vb.height / 2, vb.width / 2);
 
-  const polarToXY = (distance, angle) => {
-    const d = (distance / MAX_RADIUS) * shortest;
-    return [Math.round(d * Math.cos(angle)), Math.round(d * Math.sin(angle))];
-  };
+  const polarToXY = (distance, angle) => [
+    Math.round(distance * Math.cos(angle)),
+    Math.round(distance * Math.sin(angle)),
+  ];
 
   const mouseToXy = (ev) => [
     Math.round(((ev.pageX - svg.left) * vb.width) / svg.width + vb.minX),
@@ -52,14 +47,16 @@ window.onload = () => {
 
   document.getElementById('g').innerHTML = angles
     .map((angle, index) => {
-      const [x0, y0] = polarToXY(START_POINT_RADIUS, angle);
-      const [xc0, yc0] = polarToXY(START_CONTROL_POINT_RADIUS, angle);
-      const [x1, y1] = polarToXY(END_POINT_RADIUS, angle);
-      const [xc1, yc1] = polarToXY(END_CONTROL_POINT_RADIUS, angle);
+      const [x0, y0] = polarToXY(shortest * 0.1, angle);
+      const [xc0, yc0] = polarToXY(shortest * 0.2, angle);
+      const [x1, y1] = polarToXY(shortest, angle);
+      const [xc1, yc1] = polarToXY(shortest * 0.9, angle);
 
-      return `<path class="spline" id="p${index}"
-          d="M ${x0},${y0}
-            C ${xc0},${yc0} ${xc1},${yc1} ${x1},${y1}"
+      return `
+        <path 
+          class="spline" 
+          id="p${index}"
+          d="M ${x0},${y0} C ${xc0},${yc0} ${xc1},${yc1} ${x1},${y1}"
         />
         <line class="linea" id="l${index}" x1=${x0} y1=${y0} x2=${x0} y2=${y0} />`;
     })
@@ -68,26 +65,54 @@ window.onload = () => {
   const mouseHandler = (ev) => {
     if (ev.buttons === 0) return;
     ev.preventDefault();
-
     const [x, y] = mouseToXy(ev);
-
-    for (let index = 0; index < NUM_STEPS; index += 1) {
-      const path = document.getElementById(`p${index}`);
-
-      const [x0, y0] = polarToXY(START_POINT_RADIUS, angles[index]);
-      path.setAttribute(
-        'd',
-        path.getAttribute('d').replace(rexp, `M ${x + x0},${y + y0} $1`)
-      );
-
-      const linea = document.getElementById(`l${index}`);
-      const [x1, y1] = polarToXY(START_POINT_RADIUS, angles[index]);
-      linea.setAttribute('x1', x + x1);
-      linea.setAttribute('y1', y + y1);
-    }
     const circle = document.getElementById('c');
     circle.setAttribute('cx', x);
     circle.setAttribute('cy', y);
+    for (let index = 0; index < NUM_STEPS; index += 1) {
+      const path = document.getElementById(`p${index}`);
+      const linea = document.getElementById(`l${index}`);
+
+      switch (ev.buttons) {
+        case 1:
+          {
+            const [x0, y0] = polarToXY(shortest * 0.1, angles[index]);
+            path.setAttribute(
+              'd',
+              path
+                .getAttribute('d')
+                .replace(regex, `M ${x + x0},${y + y0} C $2 $3 $4`)
+            );
+
+            const [x1, y1] = polarToXY(shortest * 0.1, angles[index]);
+            linea.setAttribute('x1', x + x1);
+            linea.setAttribute('y1', y + y1);
+          }
+
+          break;
+        case 4:
+          {
+            const [x0, y0] = polarToXY(
+              shortest * 0.9,
+              angles[index] - STEP * (y / shortest)
+            );
+            const [x1, y1] = polarToXY(
+              shortest,
+              angles[index] + STEP * (x / shortest)
+            );
+            path.setAttribute(
+              'd',
+              path
+                .getAttribute('d')
+                .replace(regex, `M $1 C $2 ${x0},${y0} ${x1},${y1}`)
+            );
+          }
+
+          break;
+        default:
+          break;
+      }
+    }
   };
 
   window.addEventListener('mousemove', mouseHandler);
