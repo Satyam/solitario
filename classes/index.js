d62cbd6c3434a8db9e98d8de27761265e2451be7
Satyam <satyam@satyam.com.ar>
2026-07-22 18:55:37 +0200

    Moved the classes trial file from `no-jq` to its own folder `classes` Plus, added base files

---
+++ classes/index.js
@@ -0,0 +1,22 @@
+import { initBoard } from './render.js';
+import { initActions } from './actions.js';
+import { initDrag } from './dragdrop.js';
+import { initUndo } from './undoStack.js';
+import { initStats } from './stats.js';
+import { initGuess } from './guess.js';
+import { EV } from './datos.js';
+import { fireEV } from './utils.js';
+
+import { test } from './classes/test.js';
+
+export const main = () => {
+  initBoard();
+  initActions();
+  initDrag();
+  initUndo();
+  initStats();
+  initGuess();
+  test();
+
+  fireEV(EV.NEWGAME_BEFORE);
+};
