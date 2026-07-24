025a33547507fc3d8b213a395bf777e534c6c32f
Satyam <satyam@satyam.com.ar>
2026-07-24 18:08:05 +0200

    `Card` now creates the elements for the image on demand. There is one instance of `Card` for each card in the deck, but they might not be  visible all at once.  Thus, I create the `<svg>` element only when required. Added property `reverso` that tells whether it should show the face or the back of the card.

--- classes/card.js
+++ classes/card.js
@@ -1,38 +1,57 @@
-import { SVG_NS } from './constants.js';
-
-const SVG_CARD_PREFIX = 'assets/cards.svg#card_';
+import { SVG_NS, SVG_CARD_PREFIX } from './constants.js';
+import { HUECO, REVERSO } from './baraja.js';
 
 export class Card {
   #id = null;
   #el = null;
   #use = null;
   #className;
+  #reverso = false;
 
   constructor(id, className) {
     this.#id = id;
     this.#className = className;
+  }
 
+  #createEl() {
     const el = document.createElementNS(SVG_NS, 'svg');
     el.setAttribute('xmlns', SVG_NS);
     el.setAttribute('viewBox', '0 0 240 336');
-    if (className) el.classList.add('card', className);
+    el.classList.add('card');
+    if (this.#className) el.classList.add(this.#className);
 
     const use = document.createElementNS(SVG_NS, 'use');
-    use.setAttribute('href', `${SVG_CARD_PREFIX}${this.#id}`);
+    use.setAttribute(
+      'href',
+      `${SVG_CARD_PREFIX}${this.#reverso ? REVERSO : this.#id}`
+    );
     el.appendChild(use);
 
-    this.#el = el;
     this.#use = use;
+    return el;
   }
+
   set id(id) {
     if (id === this.#id) return;
     this.#id = id;
-    this.#use.setAttribute('href', `${SVG_CARD_PREFIX}${this.#id}`);
+    this.#el ??= this.#createEl();
+    this.#use.setAttribute(
+      'href',
+      `${SVG_CARD_PREFIX}${this.#reverso ? REVERSO : this.#id}`
+    );
   }
+
   get id() {
     return this.#id;
   }
+
   get el() {
-    return this.#el;
+    return (this.#el ??= this.#createEl());
+  }
+  get reverso() {
+    return this.#reverso;
+  }
+  set reverso(r) {
+    this.#reverso = r;
   }
 }
