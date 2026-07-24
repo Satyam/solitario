d334968a561d419fc2771b4fe84e78de84aed9df
Satyam <satyam@satyam.com.ar>
2026-07-24 18:09:44 +0200

    `pila.js` now displays correctly the piles of cards.

--- classes/pila.js
+++ classes/pila.js
@@ -1,21 +1,64 @@
-import { Celda } from './celda.js';
+import { Celda, TIPOS_CELDA } from './celda.js';
 import { Card } from './card.js';
-import { HUECO, CELDA } from './constants.js';
+import { HUECO } from './baraja.js';
 
 export class Pila extends Celda {
-  #card;
-  #index;
-  constructor(slot) {
-    super(CELDA.PILA, slot);
-    this.container.setAttribute('draggable', true);
-    const stk = document.createElement('div');
-    stk.classList.add('stack');
-    stk.setAttribute('draggable', true);
+  #index = 0;
+  #cardHeight;
+  #shortCardHeight;
 
-    const card = new Card(HUECO);
-    stk.appendChild(card);
-    this.container.appendChild(stk);
+  constructor(slot) {
+    super(TIPOS_CELDA.PILA, slot);
+    this.container.draggable = true;
+
+    if (this.top) {
+      this.container.classList.remove(HUECO);
+      this.update();
+    } else {
+      this.container.classList.add(HUECO);
+      this.container.draggable = false;
+    }
+    const s = getComputedStyle(document.documentElement);
+    this.#cardHeight = parseInt(s.getPropertyValue('--cardHeight'), 10);
+    this.#shortCardHeight = parseInt(
+      s.getPropertyValue('--shortCardHeight'),
+      10
+    );
   }
 
-  update() {}
+  #oneLevel(cards, container, next = false) {
+    const card = cards.pop();
+    const isVisible = cards.length <= this.#index;
+    card.reverso = !isVisible;
+    const subPila = document.createElement('div');
+    subPila.classList.add('stack');
+    if (next) subPila.classList.add('offset');
+    subPila.draggable = isVisible;
+
+    subPila.appendChild(card.el);
+    if (cards.length) {
+      this.#oneLevel(cards, subPila, true);
+    }
+    container.appendChild(subPila);
+  }
+  update() {
+    const cards = this.cards.toReversed();
+    if (cards.length) {
+      this.container.style.height =
+        ((cards.length || 1) - 1) * this.#shortCardHeight + this.#cardHeight;
+      this.container.classList.remove(HUECO);
+      this.#oneLevel(cards, this.container);
+    } else {
+      this.container.classList.add(HUECO);
+      this.container.draggable = false;
+    }
+  }
+
+  clear() {
+    super.clear();
+    this.#index = 0;
+  }
+  get visible() {
+    this.cards.filter((card, index) => index < this.#index);
+  }
 }
